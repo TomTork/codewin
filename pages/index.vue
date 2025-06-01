3a51aa262eb2b1cf0504c3aec24fdbd4a83aa263
tomtork <dima26483950@gmail.com>
2025-06-01 23:49:49 +0300

    начало создания второй страницы

--- pages/index.vue
+++ pages/index.vue
@@ -13,40 +13,41 @@ const { data: dataAdvantages, status: statusAdvantages }
   = useFetch<{id: number, name: string, value: string}[]>('https://api.los-bio.ru/info/group/advantage')
 const { data: dataProject, status: statusProject }
   = useFetch<Project[]>('https://api.los-bio.ru/projects')
+
 watch(statusSlides, () => {
-  if (statusSlides.value === 'success') {
+  if (statusSlides.value === 'success' && dataSlides.value) {
     statusLoading.value += 1;
-    slides.value = dataSlides.value ? dataSlides.value.map((it) => {
+    slides.value = dataSlides.value.map((it) => {
       return {
         id: it.id,
         name: it.name,
         value: JSON.parse(it.value) as SlideValue,
       }
-    }) : [];
+    });
   }
 })
 watch(statusAdvantages, () => {
-  if (statusAdvantages.value === 'success') {
+  if (statusAdvantages.value === 'success' && dataAdvantages.value) {
     statusLoading.value += 1;
-    advantages.value = dataAdvantages.value ? dataAdvantages.value.map((it) => {
+    advantages.value = dataAdvantages.value.map((it) => {
       return {
         id: it.id,
         name: it.name,
         value: JSON.parse(it.value) as AdvantageValue,
       }
-    }) : [];
+    });
   }
 })
 watch(statusProject, () => {
-  if (statusProject.value === 'success') {
+  if (statusProject.value === 'success' && dataProject.value) {
     statusLoading.value += 1;
-    projects.value = dataProject.value ? dataProject.value : [];
+    projects.value = dataProject.value;
     console.log(projects.value);
   }
 })
 
-function goToProject(project: Project) {
-  console.log(project);
+async function goToProject(project: Project) {
+  await navigateTo(`/project/${project.slug}`)
 }
 
 </script>
--- pages/project/[slug].vue
+++ pages/project/[slug].vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+import type { Project } from "~/types/IProject";
+
 const route = useRoute()
 const name = route.params.slug
+const project = ref<Project>({} as Project)
+const { data, status } = useFetch<Project>(`https://api.los-bio.ru/projects/${name}`)
+
+watch(status, () => {
+  if (status.value === 'success' && data.value) {
+    project.value = data.value
+  }
+})
+
 </script>
 
 <template>
